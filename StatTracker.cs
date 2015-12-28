f076c1f2326dc1a0d6f9a1b5af3f6d84e77a2d81
kugelrund <marcel.jacobse@ewetel.net>
2015-12-28 21:43:30 +0100

    Possible fix for quickloading

--- StatTracker.cs
+++ StatTracker.cs
@@ -1,9 +1,5 @@
-﻿using LiveSplit.ComponentUtil;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 
 namespace LiveSplit.Quake2_100
 {
